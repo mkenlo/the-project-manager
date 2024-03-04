249dde5a0764c2f5f63833cb89a2372d3a699bd8
Melanie <mel.lontsi@gmail.com>
2024-03-04 08:38:29 -0800

    cleaned code

--- src/main/java/com/mkenlo/theprojectmanager/controllers/TaskController.java
+++ src/main/java/com/mkenlo/theprojectmanager/controllers/TaskController.java
@@ -6,13 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.mkenlo.theprojectmanager.models.User;
 import com.mkenlo.theprojectmanager.services.TaskService;
 import com.mkenlo.theprojectmanager.services.UserService;
-
-import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class TaskController {
@@ -24,7 +21,7 @@ public class TaskController {
     TaskService taskService;
 
     @GetMapping("/tasks")
-    public String index(Model model, Principal principal) {        
+    public String index(Model model, Principal principal) {
         User user = userService.findByUsername(principal.getName());
         model.addAttribute("loggedUser", user);
         model.addAttribute("tasks", taskService.getAll());
